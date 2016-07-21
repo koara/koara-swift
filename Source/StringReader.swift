33beacaca4389393cd77b7957f5506383d4f9c9f
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-07-21 19:49:02 +0200

    start testing

--- Source/StringReader.swift
+++ Source/StringReader.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 class StringReader {
     
     var index: Int
@@ -8,8 +10,25 @@ class StringReader {
         self.text = text
     }
     
-    func read() -> Int {
-        return -1;
+    func read(buffer: [Character], offset: Int, length: Int) -> Int {
+//        if(text.substringFromIndex(text.startIndex.advancedBy(index)).characters.count > 0) {
+            var charactersRead = 0
+            for i in 0...(length - 1) {
+            
+//                var start = index + i;
+                
+//                var c = this.text.toString().substring(start, start + 1);
+//                
+//                if (c !== "") {
+//                    buffer[0] = "a"
+                    charactersRead += 1
+//                }
+            }
+            index += length;
+            return charactersRead;
+//        }
+        
+//        return -1;
     }
     
 }
--- Tests/StringReaderTest.swift
+++ Tests/StringReaderTest.swift
@@ -12,6 +12,14 @@ class StringReaderTest: XCTestCase {
     
     func testRead() {
         let reader = StringReader(text: "abcd")
-        XCTAssertEqual(reader.read(), -1)
+        XCTAssertEqual(reader.read(buffer, offset: 0, length: 4), 4)
+        //XCTAssertEqual(buffer[0], "a")
+        
+
+        //expect(buffer[1]).toEqual('b');
+        //expect(buffer[2]).toEqual('c');
+        //expect(buffer[3]).toEqual('d');
+        //expect(buffer.length).toEqual(4);
+        //expect(reader.read(buffer, 0, 4)).toEqual(-1);
     }
 }
