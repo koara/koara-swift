848ff9c652ff45aa1cd94541d0a5347988aa2cf7
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-27 15:19:46 +0100

    Add submodule

--- Tests/ComplianceTest.swift
+++ Tests/ComplianceTest.swift
@@ -2,17 +2,13 @@ import XCTest
 
 class ComplianceTest: XCTestCase {
     
-    func testKoaraToHtml5() {
-        //kd = File.read("#{TEST_DIR}/input/#{folder}/#{testcase}.kd")
-        //html = File.read("#{TEST_DIR}/output/koara/#{folder}/#{testcase}.kd")
+    
+    func testKoaraToHtml() {
+        var kd = try String(contentsOf: URL(#file), encoding: String.Encoding.utf8)
+    
         
-        let kd = "abc"
-        let html = "<p>abc</p>"
-        //let parser = Parser()
-        //let document = parser.parse(kd)
-        //let renderer = Html5Renderer()
-        //document.accept(renderer)
-        //XCTAssertEqual(html, renderer.output)
+        //kd = File.read("#{TEST_DIR}/input/#{folder}/#{testcase}.kd")
+        
     }
 
     
