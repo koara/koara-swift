2898fa1d67dfb7c81549e9db60d86ca7eb686159
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-29 17:18:33 +0100

    Retry travis build

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '8.0'
-use_frameworks!
-
-target 'Koara iOS Tests' do
-  pod 'Quick'
-  pod 'Nimble'
-end
-
--- Pods/Target Support Files/Pods-Koara iOS Tests/Pods-Koara iOS Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Koara_iOS_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Koara_iOS_TestsVersionString[];
-
