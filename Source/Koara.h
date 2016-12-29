9947e0cbbd7e6b69ff5b198468a7f63b8d4f3d55
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-29 13:57:45 +0100

    Update test to use files from module

---
+++ Source/Koara.h
@@ -0,0 +1,13 @@
+//
+//  Koara.h
+//  Koara
+//
+//  Created by Andy Van Den Heuvel on 29/12/2016.
+//
+//
+
+#ifndef Koara_h
+#define Koara_h
+
+
+#endif /* Koara_h */
--- Tests/StringReaderTests.swift
+++ Tests/StringReaderTests.swift
@@ -1,10 +1,23 @@
+import Koara
 import XCTest
 
 class StringReaderTests: XCTestCase {
     
-    func testRead() {
-         let reader = StringReader(text: "abcd")
+    var buffer = [Character]()
+    
+    override func setUp() {
+        super.setUp()
+        self.buffer = [Character]()
     }
     
-    
+    func testRead() {
+        let reader = StringReader(text: "abcd")
+        XCTAssertEqual(reader.read(&buffer, offset: 0, length: 4), 4)
+        XCTAssertEqual(buffer[0], "a")
+        XCTAssertEqual(buffer[1], "b")
+        XCTAssertEqual(buffer[2], "c")
+        XCTAssertEqual(buffer[3], "d")
+        XCTAssertEqual(buffer.count, 4)
+        XCTAssertEqual(reader.read(&buffer, offset: 0, length: 4), -1)
+    }
 }
