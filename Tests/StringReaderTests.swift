e0feacd8d23ce03ed6492cda098f16eab1b682b7
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-29 18:21:49 +0100

    Add 1 test to test coverage

--- Tests/StringReaderTests.swift
+++ Tests/StringReaderTests.swift
@@ -3,8 +3,23 @@ import XCTest
 
 class StringReaderTest: XCTestCase {
     
-    func testRead() {
-        
+    var buffer = [Character]()
+    
+    override func setUp() {
+        super.setUp()
+        self.buffer = [Character]()
     }
+    
+    func testRead() {
+        let reader = StringReader(text: "abcd")
+        XCTAssertEqual(reader.read(&buffer, offset: 0, length: 4), 4)
+        XCTAssertEqual(buffer[0], "a")
+        XCTAssertEqual(buffer[1], "b")
+        XCTAssertEqual(buffer[2], "c")
+        XCTAssertEqual(buffer[3], "d")
+        XCTAssertEqual(buffer.count, 4)
+        XCTAssertEqual(reader.read(&buffer, offset: 0, length: 4), -1)
+    }
+    
     
 }
