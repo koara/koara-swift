494d158987dbbad6a74684cfb8e06530a57e513d
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-12-29 08:55:01 +0100

    Add travis build

---
+++ Tests/ParserTests.swift
@@ -0,0 +1,19 @@
+//
+//  ParserTests.swift
+//  Koara
+//
+//  Created by Andy Van Den Heuvel on 29/12/2016.
+//
+//
+
+import XCTest
+
+class ParserTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    
+}
