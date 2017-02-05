1c8bf43825f55101dcf7c5d63f1feeaf2102eebf
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-02-05 14:24:04 +0100

    Error in middle of sentence

--- Tests/ComplianceTest.swift
+++ Tests/ComplianceTest.swift
@@ -9,8 +9,8 @@ class ComplianceTest: XCTestCase {
         //let kd = try String(contentsOf: testsuite.appendingPathComponent("input/paragraphs/paragraphs-001-simple.kd"), encoding: .utf8)
         //let html = try String(contentsOf: testsuite.appendingPathComponent("output/html5/paragraphs/paragraphs-001-simple.htm"), encoding: .utf8)
         
-        let kd = "This"
-        let html = "<p>This</p>"
+        let kd = "s a"
+        let html = "<p>s a</p>"
         
         let parser = Parser()
         let document = parser.parse(kd)
