9709b8b8c76ebdd6750131d731392408d57b8c24
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2017-04-25 07:06:02 +0200

    reset tests

--- Pods/Target Support Files/Pods-Koara macOS Tests/Pods-Koara macOS Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <Cocoa/Cocoa.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Koara_macOS_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Koara_macOS_TestsVersionString[];
-
--- Pods/Target Support Files/Pods-Koara macOS/Pods-Koara macOS-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <Cocoa/Cocoa.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Koara_macOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Koara_macOSVersionString[];
-
