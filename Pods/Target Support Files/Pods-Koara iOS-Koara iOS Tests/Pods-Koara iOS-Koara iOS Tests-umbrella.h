8f5c1f6fadb9eba8c4703735336f91a733f2a646
Andy Van Den Heuvel <andy@Andys-MacBook.local>
2017-04-27 18:46:43 +0200

    Add macOS tests

---
+++ Pods/Target Support Files/Pods-Koara iOS-Koara iOS Tests/Pods-Koara iOS-Koara iOS Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Koara_iOS_Koara_iOS_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Koara_iOS_Koara_iOS_TestsVersionString[];
+
